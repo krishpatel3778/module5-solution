c7a61fb4e0b041275bd7f514e5a49293ebb303ac
krishpatel3778 <patelrkrish@icloud.com>
2022-05-22 19:03:26 -0400

    done

--- script.js
+++ script.js
@@ -47,8 +47,82 @@ document.addEventListener("DOMContentLoaded",
                        copy=menuContent;
                 }
                $('#main-content').innerHTML=html;
-            $('#menu-tiles').innerHTML=html2;
-
+                $('#menu-tiles').innerHTML=html2;
+                singleCategory();
+            }
+    }
+    function singleCategory(){
+        console.log("here");
+        var allButtons=document.querySelectorAll("#category");
+        for(var x=0;x<allButtons.length;x++){
+            allButtons[x].addEventListener("click",function(event){
+                var url="https://davids-restaurant.herokuapp.com/menu_items.json?category="+this.getAttribute("alt");
+                console.log(url);
+                var category=this.getAttribute("type");
+                getIdata(url,category);
+            });
+        }
+        function getIdata(url,category){
+            var singleTitle;
+            var onlineIdata;
+            var singleContent;
+            ajaxUtils.sendGetRequest("snippets/singlet.html",function(request){
+                    singleTitle=request.responseText;
+                },false);
+                 ajaxUtils.sendGetRequest("snippets/singlec.html",function(request){
+                   singleContent=request.responseText;
+                },false);
+                ajaxUtils.sendGetRequest(url,function(request){
+                    onlineIdata=JSON.parse(request.responseText);
+                    buildSingle(singleTitle,singleContent,onlineIdata,category);
+                },true);
+        }
+        function buildSingle(singleTitle,singleContent,onlineIdata,category){
+                console.log(onlineIdata);
+                var html=singleTitle.replace("name",category);
+                html=html.replace('special-instructions',onlineIdata.category.special_instructions);
+                var html2="";
+                var copy=singleContent;
+                for(var i=0;i<onlineIdata.menu_items.length;i++){
+                    
+                        if(onlineIdata.menu_items[i].price_small==null){
+                            copy=copy.replace(/ps/g,"");
+                            copy=copy.replace(/small_portion/g,"");
+                        }if(onlineIdata.menu_items[i].price_large==null){
+                            copy=copy.replace(/pl/g,"");
+                            copy=copy.replace(/large_portion/g,"");
+                        }
+                        if(onlineIdata.menu_items[i].price_small!=null){
+                            copy=copy.replace(/ps/g,"$"+onlineIdata.menu_items[i].price_small);
+                            if(onlineIdata.menu_items[i].small_portion_name!=null){
+                            copy=copy.replace(/small_portion/g,onlineIdata.menu_items[i].small_portion_name);
+                            }else{
+                                copy=copy.replace(/small_portion/g,"");
+                            }
+                        }if(onlineIdata.menu_items[i].price_large!=null){
+                            copy=copy.replace(/pl/g,"$"+onlineIdata.menu_items[i].price_large);
+                            if(onlineIdata.menu_items[i].large_portion_name!=null){
+                            copy=copy.replace(/large_portion/g,onlineIdata.menu_items[i].large_portion_name);
+                            }else{
+                                copy=copy.replace(/large_portion/g,"");
+                            }
+                        }else{
+                            copy=copy.replace(/ps/g,"$"+onlineIdata.menu_items[i].price_small);
+                            copy=copy.replace(/small_portion/g,onlineIdata.menu_items[i].small_portion_name);
+                             copy=copy.replace(/pl/g,"$"+onlineIdata.menu_items[i].price_large);
+                             copy=copy.replace(/large_portion/g,onlineIdata.menu_items[i].large_portion_name);
+                        }
+                    
+                       copy=copy.replace(/same/g,onlineIdata.menu_items[i].short_name);
+                       copy=copy.replace(/name/g,onlineIdata.menu_items[i].name);
+                       copy=copy.replace(/descriptions/g,onlineIdata.menu_items[i].description);
+                       copy=copy.replace(/categoryy/g,onlineIdata.category.short_name);
+                       copy=copy.replace(/categories/g,onlineIdata.menu_items[i].short_name);
+                       html2+=copy;
+                       copy=singleContent;
+                }
+               $('#main-content').innerHTML=html;
+                $('#menu-tiles').innerHTML=html2;
             }
     }
 }
